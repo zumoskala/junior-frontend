f1792abaa11576a343609936cea7ddac71339f98
Zuzanna <zuzannamoskala@gmail.com>
2020-11-01 17:56:09 +0100

    task 3 - final display fetched data from github API

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -3,34 +3,43 @@ import React, {Component} from 'react';
 class User extends Component {
     constructor(props) {
         super(props);
+
         this.state = {
-            username: 'username',
-            avatar: 'avatar'
-        }
+            username : 'zumoskala',
+            items: [] };
+    }
+
+    componentDidMount() {
+        fetch(`https://api.github.com/users/${this.state.username}`)
+            .then(res => res.json())
+            .then( data => this.setState({ items : data }) );
     }
 
     render() {
+
+        const { items } = this.state;
+
         const styles = {
             userName: {
                 textDecoration: 'none',
                 color: '#000000',
                 padding: '1.5rem 0',
-                fontSize: '1.5rem',
+                fontSize: '2rem',
                 fontWeight: 'bold'
             },
             avatar: {
-                width: '350px',
-                height: '350px',
-                border: '2px solid black',
-                borderRadius: '10px'
+                width: '300px',
+                height: '300px',
+                borderRadius: '10px',
+                boxShadow: '0px 0px 123px 8px rgba(0,0,0,0.3)'
             }
         }
 
         return (
             <div>
-                <div className="userName" style={styles.userName}>LOGIN</div>
-                <div className="avatar" style={styles.avatar}>
-                    <p>{this.state.username}</p>
+                <div className="userName" style={styles.userName}>{ items.login }</div>
+                <div className="avatar">
+                    <img style={styles.avatar} src={ items.avatar_url } />
                 </div>
             </div>
         )
